3edd89c806e01ba257a22f5cd3fbf3f60f6e7ca6
nmtcode <nmtcode@example.com>
2025-10-02 20:17:39 +0300

    تحديث الصفحات وإضافة الشعار

--- assest/index.js
+++ assest/index.js
@@ -1,26 +1,26 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    // تشغيل القائمة في الجوال (لزر التبديل)
+
+console.log('yeet');
+document.addEventListener('DOMContentLoaded', () => {
+    // زر القائمة للجوال
     const menuToggle = document.querySelector('.menu-toggle');
     const navbar = document.querySelector('.navbar');
     menuToggle.addEventListener('click', () => {
         navbar.classList.toggle('active');
     });
 
-    // تشغيل تحريك Fade-in و Slide-up
-    const observer = new IntersectionObserver((entries) => {
+    // تحريك Fade-in و Slide-up
+    const observer = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 if (entry.target.classList.contains('fade-in')) {
-                    entry.target.style.animationName = 'fadeIn';
+                    entry.target.style.animation = 'fadeIn 0.8s forwards ease-out';
                 } else if (entry.target.classList.contains('slide-up')) {
-                    entry.target.style.animationName = 'slideUp';
+                    entry.target.style.animation = 'slideUp 0.8s forwards ease-out';
                 }
                 observer.unobserve(entry.target);
             }
         });
-    }, { threshold: 0.1 }); // تفعيل التحريك عند ظهور 10% من العنصر
+    }, { threshold: 0.1 });
 
-    document.querySelectorAll('.fade-in, .slide-up').forEach(element => {
-        observer.observe(element);
-    });
+    document.querySelectorAll('.fade-in, .slide-up').forEach(el => observer.observe(el));
 });
